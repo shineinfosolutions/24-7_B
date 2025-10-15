06b2f5b854a35f71311b75f3a83992af69471223
Saumya Singh <166956447+SaumyaSingh04@users.noreply.github.com>
2025-10-15 13:26:57 +0530

    dd

--- server.js
+++ server.js
@@ -34,11 +34,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-<<<<<<< HEAD
-  origin: ['http://localhost:5173', 'https://zomato-frontend-blush.vercel.app'],
-=======
   origin: ['http://localhost:5173','zomato-frontend-blush.vercel.app'],
->>>>>>> 6a2853c23e9c6853d2401b2babe6edbce184fd73
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
