81a748ae3efffea48541932fe57fb341bb323562
Sayain-Aditya <as4316664@gmail.com>
2025-10-15 12:07:54 +0530

    asd

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173','https://24-7-b.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
