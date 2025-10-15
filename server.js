048ba9808663f7ba00288e0575a9719906201ade
Saumya Singh <166956447+SaumyaSingh04@users.noreply.github.com>
2025-10-15 13:28:06 +0530

    dd

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ['http://localhost:5173','zomato-frontend-blush.vercel.app'],
+  origin: ['http://localhost:5173','http://zomato-frontend-blush.vercel.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
