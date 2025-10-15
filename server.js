644473969103097cb686eaeaaad5f8dc981c83cc
Sayain-Aditya <as4316664@gmail.com>
2025-10-15 11:52:10 +0530

    Remove unnecessary origin from CORS configuration

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ['http://localhost:5173','zomato-frontend-blush.vercel.app'],
+  origin: ['http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
