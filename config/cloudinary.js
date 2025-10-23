f4910fd60811a27aca5492a5a527c8f628616880
Saumya Singh <166956447+SaumyaSingh04@users.noreply.github.com>
2025-10-23 13:12:14 +0530

    ..

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,16 +1,9 @@
 import { v2 as cloudinary } from 'cloudinary';
-import dotenv from 'dotenv';
 
-dotenv.config();
-
-try {
-  cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
-  });
-} catch (error) {
-  console.error('Cloudinary configuration failed:', error);
-}
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 export default cloudinary;
